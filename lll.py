a3c90dbe2e05a8505f050e3a0e599c03d549ad2b
egornagornov <egornagornov2009@gmail.com>
2024-06-18 18:48:39 +0300

    better

--- lll.py
+++ lll.py
@@ -1,7 +1,24 @@
-print('hello')
-print('he')
+import sys
+import pygame
 
-# комментарий
+pygame.init()
+screen = pygame.display.set_mode((600, 300))
+pygame.display.set_caption("Egors game")
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
 
-#gjggjgjgjgjgjgjgjg
-# еще какой-то коментарий
+running = True
+
+while running:
+    # screen.fill('#34ebc3')
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                screen.fill("#eb3459")
+
